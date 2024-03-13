df9b37c0a59f0a351c5a612ca4ba2f131670ea15
israaayman <emanayman657@yahoo.com>
2024-03-13 17:37:27 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 //choosen color /string
 var choosenColor="red";
-console.log(choosenColor);
+console.log(choosenColor ,typeof choosenColor);
 
 //subscribed to a newsletter /boolean
 var isSupscribed= true;
-console.log(isSupscribed);
+console.log(isSupscribed ,typeof isSupscribed);
 
 //Calculating the area of a rectangle /number
 var CalculatinRectangle= 25;
-console.log(CalculatinRectangle);
+console.log(CalculatinRectangle ,typeof CalculatinRectangle);
 
 //a welcome message /string
 var welcomeMessage="Welcome";
-console.log(welcomeMessage);
+console.log(welcomeMessage ,typeof welcomeMessage);
 
 //Checking password /number
 var password= 123456789;
-console.log(password);
+console.log(password ,typeof password);
